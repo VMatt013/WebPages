ae97cc8c31f75e9356cf43204cd7a80235958198
VMatt013 <v.matt0826@gmail.com>
2022-02-07 21:21:36 +0100

    Dino update

---
+++ Dinoszauruszok/Script.js
@@ -0,0 +1,299 @@
+const Dino  = document.querySelector('#dino-1')
+const Dino2 = document.querySelector('#dino-2')
+const Dino3 = document.querySelector('#dino-3')
+const Dino4 = document.querySelector('#dino-4')
+const Dino5 = document.querySelector('#dino-5')
+const Dino6 = document.querySelector('#dino-6')
+const Dino7 = document.querySelector('#dino-7')
+const Dino8 = document.querySelector('#dino-8')
+const Dino9 = document.querySelector('#dino-9')
+const Dino0 = document.querySelector('#dino-0')
+var isOn = false;
+
+Dino.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-1").style.display = "block";
+    }
+});
+
+Dino.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-1").style.display = "none";
+    }
+});
+
+Dino.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-1").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-1").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino2.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-2").style.display = "block";
+    }
+});
+
+Dino2.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-2").style.display = "none";
+    }
+});
+
+Dino2.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-2").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-2").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino3.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-3").style.display = "block";
+    }
+});
+
+Dino3.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-3").style.display = "none";
+    }
+});
+
+Dino3.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-3").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-3").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino4.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-4").style.display = "block";
+    }
+});
+
+Dino4.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-4").style.display = "none";
+    }
+});
+
+Dino4.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-4").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-4").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino5.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-5").style.display = "block";
+    }
+});
+
+Dino5.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-5").style.display = "none";
+    }
+});
+
+Dino5.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-5").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-5").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino6.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-6").style.display = "block";
+    }
+});
+
+Dino6.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-6").style.display = "none";
+    }
+});
+
+Dino6.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-6").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-6").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino7.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-7").style.display = "block";
+    }
+});
+
+Dino7.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-7").style.display = "none";
+    }
+});
+
+Dino7.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-7").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-7").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino8.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-8").style.display = "block";
+    }
+});
+
+Dino8.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-8").style.display = "none";
+    }
+});
+
+Dino8.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-8").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-8").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino9.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-9").style.display = "block";
+    }
+});
+
+Dino9.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-9").style.display = "none";
+    }
+});
+
+Dino9.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-9").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-9").style.display = "block";
+        isOn = true;
+    }
+});
+
+
+
+Dino0.addEventListener("mouseover",() => {
+    if(!isOn)
+    {
+    document.getElementById("dino-holder-0").style.display = "block";
+    }
+});
+
+Dino0.addEventListener("mouseout",() => {
+    if(!isOn)
+    {
+        document.getElementById("dino-holder-0").style.display = "none";
+    }
+});
+
+Dino0.addEventListener("click",() => {
+    if(isOn)
+    {
+        document.getElementById("dino-holder-0").style.display = "none";
+        isOn = false;
+    }
+    else
+    {
+        document.getElementById("dino-holder-0").style.display = "block";
+        isOn = true;
+    }
+});
