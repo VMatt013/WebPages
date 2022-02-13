010f5436570d4201b40bf49d9456bca0d16af454
VMatt013 <v.matt0826@gmail.com>
2022-02-13 12:30:08 +0100

    Dorona update

---
+++ Mate-Dorona/Quotes.js
@@ -0,0 +1,37 @@
+Quotes = 
+[
+    /*0*/  "The fact that you have a place where you can return home, will lead you to happiness. That is a good fact.",
+    /*1*/  "If you think you can be my one and only true love you must promise to love me",
+    /*2*/  "You will be my girl",
+    /*3*/  "Gotta stay high all my life <br> To forget I'm missin' you",
+    /*4*/  "Heaven is a place on earth with you",
+    /*5*/  "Now I need your help with everything that I do <br> I don't want to lie, I've been relying on you",
+    /*6*/  "Your skin is warm like an oven <br> Your kiss is sugary sweet <br> Your fingers feel like cotton <br> When you put your arms around me",
+    /*7*/  "I think we could do it if we tried if only to say ''You're mine''",
+    /*8*/  "Are you into me, like I'm into you? <br> Do you wanna do the things I wanna do with you?",
+    /*9*/  "Kiss me hard before you go <br> Summertime sadness",
+    /*10*/ "I don't like anyone better than you, it's true",
+    /*1*/  "'Cause you're my religion <br> You're how I'm living",
+    /*2*/  "We'll do whatever you want, travel wherever how far",
+    /*3*/  "It hurts to love you, but I still love you <br> It's just the way I feel <br> And I'd be lying if I kept hiding",
+    /*4*/  "No one knows I cry in my sleep <br> Waking up, feeling like shit <br> It's normal thing to feel like this",
+    /*5*/  "Everyday, if you leave, I'll be weak by the end of the week <br> And sometimes we don't get along and I need some time alone <br> Try to make it on my own, I go crazy when you're gone", 
+    /*6*/  "You and me always forever <br> We could stay alone together"
+    /*7*/
+    /*8*/
+    /*9*/
+    /*20*/
+    /*1*/
+    /*2*/
+    /*3*/
+    /*4*/
+    /*5*/
+    /*6*/
+    /*7*/
+    /*8*/
+    /*9*/
+    /*30*/ 
+]
+num = (Math.random()*16).toFixed(0);
+console.log(num);
+document.getElementById("Quote").innerHTML=Quotes[num];
